ba79e2611c23664afdbb5071947793409bef1ab7
adamPetho <45069029+adamPetho@users.noreply.github.com>
2025-11-27 19:33:19 +0100

    add Environment Helper to create and find data folder

---
+++ OnionSharp/Helpers/EnvironmentHelpers.cs
@@ -0,0 +1,45 @@
+﻿using System.Runtime.InteropServices;
+
+namespace OnionSharp.Helpers
+{
+    public static class EnvironmentHelpers
+    {
+        public static string GetDataDir(string appName)
+        {
+            string directory;
+
+            if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                var home = Environment.GetEnvironmentVariable("HOME");
+                if (!string.IsNullOrEmpty(home))
+                {
+                    directory = Path.Combine(home, "." + appName.ToLowerInvariant());
+                }
+                else
+                {
+                    throw new DirectoryNotFoundException("Could not find suitable datadir.");
+                }
+            }
+            else
+            {
+                var localAppData = Environment.GetEnvironmentVariable("APPDATA");
+                if (!string.IsNullOrEmpty(localAppData))
+                {
+                    directory = Path.Combine(localAppData, appName);
+                }
+                else
+                {
+                    throw new DirectoryNotFoundException("Could not find suitable datadir.");
+                }
+            }
+
+            if (Directory.Exists(directory))
+            {
+                return directory;
+            }
+
+            Directory.CreateDirectory(directory);
+            return directory;
+        }
+    }
+}
